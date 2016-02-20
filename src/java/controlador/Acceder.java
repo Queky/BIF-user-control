b2dd3da29dd30d0f2ce90132186d8a6578e946ef
Queky <inakisanchez_92@hotmail.com>
2016-02-20 21:00:08 +0100

    cambios

---
+++ src/java/controlador/Acceder.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import modelo.conexion.Conexion;
+
+/**
+ *
+ * @author inakisanchez
+ */
+public class Acceder extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession s = request.getSession();
+        String correo = request.getParameter("email");
+        String contrasena = request.getParameter("contrasena");
+        Conexion c = new Conexion();
+        String nombre;
+        try {
+            if(c.comprobarUsuario(correo, contrasena)){
+                nombre = c.obtenerNombre(correo);
+                s.setAttribute("usuario", nombre);
+                s.setAttribute("logged", true);
+                request.getRequestDispatcher("JSP/PaginaUsuario.jsp").forward(request, response);
+            }
+            else{
+                s.setAttribute("existe", "noExiste");
+                request.getRequestDispatcher("JSP/PaginaInicio.jsp").forward(request, response);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Acceder.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/JSP/GestionCompeticion.jsp
+++ web/JSP/GestionCompeticion.jsp
@@ -10,8 +10,84 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Gestion de la competición</title>
+        <link href="../CSS/Estilo.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="../CSS/bootstrap.min.css">
+        <script src="../js/jquery.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
     </head>
     <body>
-        <h2>Gestion de la competición</h2>
+        <header style="text-align: center">
+            <h1>Bizkaiko Igeriketa Federazioa</h1>
+        </header>
+        <div style="text-align: center">
+            <h3>Gestion de la competición</h3>
+        </div>
+        <section class="centrarSection">
+            <table class="table-responsive center-block table-bordered">
+                <tr>
+                    <td>
+                        <p>Juez arbitro:</p>
+                    </td>
+                    <td>
+                        
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Juez de salidas:</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Juez de estilos:</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Juez de estilos:</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Secretaria:</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Calle 1:</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Calle 2:</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Calle 3:</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Calle 4:</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Calle 5:</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Calle 6:</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>Jefe de cronos:</p>
+                    </td>
+                </tr>
+            </table>
+        </section>
     </body>
 </html>
